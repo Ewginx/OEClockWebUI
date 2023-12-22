7543c5af5e981a733db741886a604b5695a6536b
Evgeniy <ewginx@gmail.com>
2023-12-22 23:42:56 +1000

    enable historyApiFallback

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
     static: {
       directory: path.join(__dirname, 'dist')
     },
-    // historyApiFallback: true,
+    historyApiFallback: true,
     open: true,
   },
   devtool: 'inline-source-map',
