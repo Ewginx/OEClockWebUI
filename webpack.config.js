58fe8fd99764774f416b6c6a93881a74ff024771
Evgeniy <ewginx@gmail.com>
2023-12-29 17:19:04 +1000

    configure copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, './src/index.js'),
+    main: path.resolve(__dirname, "./src/index.js"),
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: 'index.html',
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
       // inject: false,
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'assets/webfonts'), to: path.resolve(__dirname, 'dist/webfonts') },
+      ],
     }),
   ],
   optimization: {
@@ -26,24 +31,32 @@ module.exports = {
       {
         test: /\.html$/i,
         loader: "html-loader",
-        generator : {
-          filename : '[name][ext]',
-        }
+        generator: {
+          filename: "[name][ext]",
+        },
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-        generator : {
-          filename : '[name][ext]',
-        }
+        type: "asset/resource",
+        generator: {
+          filename: "[name][ext]",
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)$/,
+        type: "asset/source",
+        generator: {
+          filename: "./webfonts/[name][ext]",
+        },
       },
       {
         test: /\.css$/i,
         // use: ["style-loader", "css-loader"],
-        generator : {
-          filename : '[name][ext]',
-        }
+        generator: {
+          filename: "[name][ext]",
+        },
       },
+
     ],
   },
-}
+};
