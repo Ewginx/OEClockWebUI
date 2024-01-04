65144cc555012307c15162d686b9015b046bd27f
Evgeniy <ewginx@gmail.com>
2024-01-05 05:28:39 +1000

    fix mismatched hash in filename and html assets

--- webpack.prod.js
+++ webpack.prod.js
@@ -36,6 +36,7 @@ module.exports = merge(common, {
   optimization: {
     minimize: true,
     minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    realContentHash: false,
   },
   module: {
     rules: [
